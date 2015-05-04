05f2cefd097dead24f0e92f700de13f89b1f68df
kyle <pseudobyte@gmail.com>
2015-05-04 15:53:42 -0600

    translated execution engine to class

--- fuckwit.py
+++ fuckwit.py
@@ -27,31 +27,57 @@ else:
     else: ch = ord(ch)
     return ch
 
+class ExecutionEngine(object):
+  def __init__(self, ops = None, memsize = 30000):
+    object.__init__(self)
+
+    self.mem = mem = np.ndarray(memsize, dtype=np.uint8)
+    self.memsize = memsize
+    self.ip = 0
+    self.mp = 0
+    self.ops = []
+
+    if ops:
+      self.addops(ops)
+
+    self.handlers = [self.val, self.ptr, self.jmp, self.inp, self.out]
+
+  def addops(self, ops):
+    self.ops += ops
+
+  def run(self):
+    while self.ip != len(self.ops):
+      self.handle(self.ops[self.ip])
+
+  def handle(self, op):
+    self.handlers[op[0]](*op[1:])
+    self.ip += 1
+
+  def val(self, delta):
+    self.mem[self.mp] += delta
+
+  def ptr(self, delta):
+    self.mp = (self.mp + delta) % self.memsize
+
+  def jmp(self, cond, delta):
+    if (self.mem[self.mp] == 0) == cond:
+      self.ip += delta
+
+  def inp(self):
+    self.mem[self.mp] = getch()
+
+  def out(self):
+    c = self.mem[self.mp]
+    if c > 31 and c < 127:
+      sys.stdout.write(chr(c))
+    elif c == 10 or c == 13:
+      sys.stdout.write('\n')
+
 fp = open(sys.argv[1])
 dat = fp.read()
 fp.close()
 
 ops = parser.parse(dat)
 
-mem = np.ndarray(memsize, dtype=np.uint8)
-ip = 0
-ptr = 0
-
-while ip != len(ops):
-  op = ops[ip]
-  if op[0] == 0:
-    mem[ptr] += op[1]
-  elif op[0] == 1:
-    ptr = (ptr + op[1]) % memsize
-  elif op[0] == 3:
-    mem[ptr] = getch()
-  elif op[0] == 4:
-    c = mem[ptr]
-    if c > 31 and c < 127:
-      sys.stdout.write(chr(c))
-    if c == 10 or c == 13:
-      sys.stdout.write('\n')
-  elif op[0] == 2:
-    if (mem[ptr] == 0) == op[1]:
-      ip += op[2]
-  ip += 1
+ee = ExecutionEngine(ops)
+ee.run()
